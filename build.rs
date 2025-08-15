d91dcde635c8f8a5b3d27e4d36b998d5b2a9cb8d
Andrius Bentkus <andrius.bentkus@gmail.com>
2025-08-15 18:36:15 +0300

    use c++ lib on macos

--- build.rs
+++ build.rs
@@ -6,6 +6,15 @@ fn main() {
     let mut bridge = cxx_build::bridge(src.join("bridge.rs"));
 
     assert!(bridge.is_flag_supported("-std=c++11").expect("supported"));
+
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    let stdlib = if target_os == "macos" {
+        "c++"
+    } else {
+        "stdc++"
+    };
+
     bridge
         .files(&[
             datasketches.join("cpc.cpp"),
@@ -14,7 +23,7 @@ fn main() {
         ])
         .include(datasketches.join("common").join("include"))
         .flag_if_supported("-std=c++11")
-        .cpp_link_stdlib("stdc++")
+        .cpp_link_stdlib(stdlib)
         .static_flag(true)
         .compile("libdatasketches.a");
 }
